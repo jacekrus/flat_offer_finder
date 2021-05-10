decf5fc090f6a0b21e9ff8aedab01ee57f70bce6
jacekrus <jacek1807@gmail.com>
2021-05-10 23:13:40 +0200

    Added headless run option, fixed cookies agreement on Morizon

--- src/main/java/flat/offer/Main.java
+++ src/main/java/flat/offer/Main.java
@@ -12,34 +12,34 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Main {
 
-	private static final Logger LOG = Logger.getLogger(Main.class);
+    private static final Logger LOG = Logger.getLogger(Main.class);
 
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER"));
-		ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(1);
-		scheduledExecutor.scheduleAtFixedRate(Main::scanOffers, 0, 15, TimeUnit.MINUTES);
-	}
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER"));
+        ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(1);
+        scheduledExecutor.scheduleAtFixedRate(() -> scanOffers(args), 0, 15, TimeUnit.MINUTES);
+    }
 
-	private static void scanOffers() {
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless", "--window-size=1920,1200");
-		ChromeDriver webDriver = new ChromeDriver(options);
-		try {
-			List<OfferFinder> finders = List.of(new OtoDomOfferFinder(webDriver), new MorizonOfferFinder(webDriver));
-			OfferEvaluator offerEvaluator = new OfferEvaluator(new OfferFileStorageHandler(), new MailSender());
-			for (OfferFinder finder : finders) {
-				LOG.info("Starting to look for new offers on " + finder.getSite().toString() + "...");
-				Collection<Offer> offers = finder.getOffers();
-				offerEvaluator.evaluateFoundOffers(finder.getSite(), offers);
-			}
-			LOG.info("Finishing execution...");
-		}
-		catch(Exception e) {
-			LOG.error(e.getMessage(), e);
-		}
-		finally {
-			webDriver.close();
-		}
-	}
+    private static void scanOffers(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        if (args != null && args.length > 0 && args[0].equals("-h")) {
+            options.addArguments("--headless", "--window-size=1920,1200");
+        }
+        ChromeDriver webDriver = new ChromeDriver(options);
+        try {
+            List<OfferFinder> finders = List.of(new OtoDomOfferFinder(webDriver), new MorizonOfferFinder(webDriver));
+            OfferEvaluator offerEvaluator = new OfferEvaluator(new OfferFileStorageHandler(), new MailSender());
+            for (OfferFinder finder : finders) {
+                LOG.info("Starting to look for new offers on " + finder.getSite().toString() + "...");
+                Collection<Offer> offers = finder.getOffers();
+                offerEvaluator.evaluateFoundOffers(finder.getSite(), offers);
+            }
+            LOG.info("Finishing execution...");
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            webDriver.close();
+        }
+    }
 
 }
